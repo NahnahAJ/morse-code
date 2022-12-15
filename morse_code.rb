e644334c4ea65651a9cd658bdd20f0e7a36c045d
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-15 13:24:30 +0000

    Add code to decode morse code message

---
+++ morse_code.rb
@@ -0,0 +1,43 @@
+# Create a method to decode a Morse code character,
+# takes a string parameter, and return the corresponding character in uppercase
+
+# Morse code translation list
+@translations = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
+  '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+  '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+}
+
+# Create a method to decode a Morse code character
+def decode_char(morse_code)
+  # Your code here
+  @translations[morse_code]
+end
+# (e.g. decode_char(".-") returns "A").
+decode_char('.-')
+
+# Create a method to decode an entire word in Morse code,
+# takes a string parameter, and return the string representation.
+# Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
+def decode_word(morse_word)
+  # Your code here
+  morse_word = morse_word.split
+  morse_word = morse_word.map { |char| decode_char(char) }
+  morse_word = morse_word.join
+  print morse_word
+end
+decode_word('-- -.--')
+
+# Create a method to decode the entire message in Morse code,
+# takes a string parameter, and return the string representation.
+# Every word will be separated by 3 spaces (e.g.   decode("-- -.--   -. .- -- .") returns "MY NAME").
+def decode(morse_message)
+  # Your code here
+  morse_message = morse_message.split
+  morse_message = morse_message.map { |word| decode_word(word) }
+  morse_message = morse_message.join
+  puts morse_message
+end
+decode('-- -.--   -. .- -- .')
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
